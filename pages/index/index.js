d6ffe6ed6b3c42257821cb4fc4fa55cdb2a8052b
Ruvik <1092045998@qq.com>
2020-02-07 19:52:42 +0800

    社区初步UI

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,43 @@ Page({
 
   },
 
+  onChange(e) {
+    this.setData({
+      value: e.detail
+    });
+  },
+
+  onSearch() {
+    if (this.data.value) {
+      wx.showToast({
+        title: '搜索：' + this.data.value,
+        icon: 'none'
+      });
+    }
+  },
+
+  onClick() {
+    if (this.data.value) {
+      wx.showToast({
+        title: '搜索：' + this.data.value,
+        icon: 'none'
+      });
+    }
+  },
+
+  onCancel() {
+    wx.showToast({
+      title: '取消',
+      icon: 'none'
+    });
+  },
+
+  onClear() {
+    wx.showToast({
+      title: '清空',
+      icon: 'none'
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
