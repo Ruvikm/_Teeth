e7cf5ce4c9b3ab4a5d60777c6eedeac4acd0a357
Ruvik <1092045998@qq.com>
2020-02-07 13:59:04 +0800

    大量优化

--- pages/data/data.js
+++ pages/data/data.js
@@ -10,22 +10,11 @@ Page({
     //判断小程序的API，回调，参数，组件等是否在当前版本可用。
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     isHide: false,
-    show: {
-      basic: false,
-      top: false,
-      bottom: false,
-      left: false,
-      right: false,
-      round: false,
-      closeIcon: false,
-      customCloseIcon: false,
-      customIconPosition: false,
-    },
   },
   toggle(type, show) {
     this.setData({
       [`show.${type}`]: show
-    });
+    })
   },
   showRound() {
     this.toggle('round', true);
