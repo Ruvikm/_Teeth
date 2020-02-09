401405ea77bb62187713f9992b7f98a6fc502124
Ruvik <1092045998@qq.com>
2020-02-09 21:09:55 +0800

    修补了个人信息填的bug

--- pages/information/information.js
+++ pages/information/information.js
@@ -2,6 +2,10 @@
 
 //#region 数据的初始化
 
+const APP_ID ='wxcfcae2873cc82924';//输入小程序appid  
+const APP_SECRET ='a29bc7c43657e6ecddf66518ac47aa3f';//输入小程序app_secret  
+var OPEN_ID=''//储存获取到openid  
+var SESSION_KEY=''//储存获取到session_key 
 const db = wx.cloud.database();
 const CaseCollection = db.collection('Information');
 var userName;
@@ -9,11 +13,47 @@ var usersex;
 var userage;
 var userphone;
 var usertext;
+var flag = true;
 
 //#endregion
 
 
 Page({
+
+
+//#region 获取openid
+
+  getOpenIdTap:function(){  
+    var that=this;  
+    wx.login({  
+      success:function(res){  
+        wx.request({  
+            //获取openid接口  
+          url: 'https://api.weixin.qq.com/sns/jscode2session',  
+          data:{  
+            appid:APP_ID,  
+            secret:APP_SECRET,  
+            js_code:res.code,  
+            grant_type:'authorization_code'  
+          },  
+          method:'GET',  
+          success:function(res){  
+            console.log(res.data)  
+            OPEN_ID = res.data.openid;//获取到的openid  
+            SESSION_KEY = res.data.session_key;//获取到session_key  
+            console.log(OPEN_ID.length)  
+            console.log(SESSION_KEY.length)  
+            that.setData({  
+              openid: res.data.openid.substr(0, 10) + '********' + res.data.openid.substr(res.data.openid.length - 8, res.data.openid.length),  
+              session_key: res.data.session_key.substr(0, 8) + '********' + res.data.session_key.substr(res.data.session_key.length - 6, res.data.session_key.length)  
+            })  
+          }  
+        })  
+      }  
+    })  
+  },
+
+  //#endregion
 
   /**
    * 页面的初始数据
@@ -26,14 +66,15 @@ Page({
     usertext:''
   },
   
-  //#region 保存input里的内容并上传到服务器的数据库里
+  //#region 保存input里的内容并上传到服务器的数据库里，如果用户只是修改数据，则单独修改数据库里变化的数据
 
   
   Save:function () {
+    //#region 获取用户的输入信息
+
     //获取姓名
-    
-    // 通过实例调用组件事件
     let Component_Name = this.selectComponent('#Name'); // 页面获取自定义组件实例
+    // 通过实例调用组件事件
     userName =Component_Name.data.value;
     console.log(userName);
 
@@ -61,22 +102,48 @@ Page({
     usertext =Component_Text.data.value;
     console.log(usertext);
 
-    //上传到数据库
-    db.collection('Information').add({
-      // data 字段表示需新增的 JSON 数据
-      data: {
-         _id: "", // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
-        Name: userName,
-        Age: userage,
-        Sex:usersex,
-        Phone:userphone,
-        Sign:usertext
-      },
-      success: function (res) {
-        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
-        console.log(res)
-      }
-    })
+    //#endregion
+
+    //上传到数据库，前提是用户之前在数据库里没有记录
+
+    if(flag)
+    {
+        db.collection('Information').add({
+        // data 字段表示需新增的 JSON 数据
+        data: {
+          _id: "", // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
+          Name: userName,
+          Age: userage,
+          Sex:usersex,
+          Phone:userphone,
+          Sign:usertext
+        },
+        success: function (res) {
+          // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+          console.log("添加成功")
+        }
+      })
+    }
+
+    //否则就修改那个人的更改的信息
+
+    else{
+      db.collection('Information').where({
+        _openid: OPEN_ID,
+      }).update({
+        // data 传入需要局部更新的数据
+        data: {
+          Name: userName,
+          Age: userage,
+          Sex:usersex,
+          Phone:userphone,
+          Sign:usertext
+        },
+        success: function(res) {
+          console.log("修改成功")
+        }
+      })
+    }
 
   },
 
@@ -89,8 +156,9 @@ Page({
   //#region 页面加载时用数据库获取当前用户的数据
 
   onLoad: function (options) {
+    console.log(OPEN_ID);
     db.collection('Information').where({
-      _openid: 'oedX74iTLzf9rF4-Z3LKXqIglGXI',
+      _openid: OPEN_ID,
     })
     .get({
       success: function(res) {
@@ -101,7 +169,7 @@ Page({
         userage=res.data[0].Age;
         userphone=res.data[0].Phone;
         usertext=res.data[0].Sign;
-        console.log(userName);
+        flag = false;
       }
     })
     
